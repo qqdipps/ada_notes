1b5c8cd4940fee3f73dfc417feb538ef853c3e59
qqdipps <sdipp007@gmail.com>
2019-02-25 10:44:24 -0800

    added vocab and updated tet.rb for product example

--- week3/TODAY/test.rb
+++ week3/TODAY/test.rb
@@ -1,13 +1,29 @@
 class Product
   def name
-    return "Spanx"
+    return @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def quantity_in_stock=(new_quantity_in_stock)
+    @quantity_in_stock = new_quantity_in_stock
   end
 
   def quantity_in_stock
-    return 547
+    return @quantity_in_stock
   end
 
   def available?
     return quantity_in_stock > 0
   end
 end
+
+p1 = Product.new
+p1.name = "Spuds"
+p1.quantity_in_stock = 547
+
+puts p1.name
+puts p1.quantity_in_stock
+puts p1.available?
